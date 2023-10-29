407f7f64d95761025c951331e5a6df552a476993
molliessss <134296443+molliessss@users.noreply.github.com>
2023-10-29 21:18:20 +0000

    Uploading p5*js files from desktop
    green text interactive piece

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+ 
+}
+
+function draw() {
+   let inside = color(2, 200, 89)
+   let p = createP('hi Im mollie and im trying really hard to code');
+  p.style('font-size', '12px');
+p.style('width', '65px');
+p.style('text-align', 'center');
+p.position(mouseX, mouseY, 100);
+  if (mouseIsPressed) {
+ fill(0);
+  }else{
+    fill(inside);
+}
+createP(mouseX, mouseY, 100) 
+  square(mouseX, mouseY, 100)
+}
